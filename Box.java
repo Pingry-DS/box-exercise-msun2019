dc2ea82aa186c20b607d5235cb6b7ae7b3792040
Michael Sun <msun2019@pingry.org>
2017-09-14 17:11:07 -0400

    Finished read me and Box program

--- Box.java
+++ Box.java
@@ -33,7 +33,7 @@ public class Box<T>{
   }
 
   /**
-   * Empty out the box, and give back hatever was in it
+   * Empty out the box, and give back whatever was in it
    */
   public T empty(){
     T junk = contents;
@@ -41,10 +41,22 @@ public class Box<T>{
     isFull = false;
     return junk;
   }
-
+  /**
+   *
+   *
+   *
+   */
+  public boolean fill(T contents){
+    if (isFull){
+      return false;
+    }
+    this.contents = contents;
+    isFull = true;
+    return isFull;
+  }
   public String toString(){
     if (isFull){
-      return "This box contains" + contents;
+      return "This box contains " + contents;
     }
     else{
       return "This box is empty";
@@ -55,27 +67,26 @@ public class Box<T>{
 
     // Make two boxes
     Box<String> stringBox = new Box<String>();
-    //TODO start the second box with contents inside
-    Box<Integer> intBox   = new Box<Integer>();
+
+    Box<Integer> intBox   = new Box<Integer>(42);
 
     // View contents (via toString method)
     System.out.println("The boxes contain: " + stringBox + ", " + intBox);
 
-    //TODO Add something to a box
-
-
-
-    // View contents (via toString method)
-    System.out.println("The boxes contain: " + stringBox + ", " + intBox);
-
-    //TODO Test your peek method
+    stringBox.fill("Hello World");
 
 
     // View contents (via toString method)
     System.out.println("The boxes contain: " + stringBox + ", " + intBox);
+
+
+
+    // View contents (via toString method)
+    System.out.println("The boxes contain: " + stringBox.getContents() + ", " + intBox.getContents());
 
     //TODO Empty a boxes
-
+    stringBox.empty();
+    intBox.empty();
 
 
     // View contents (via toString method)
